f8de1fb2adde6f8e01e2af38e028f975685c37c6
Jeff Higgason <jhiggy@gmail.com>
2023-09-28 08:53:25 -0700

    update to tank_control to use lgpio instead of rpigpio

--- src/tank_control_pkg/tank_control_pkg/tank_control.py
+++ src/tank_control_pkg/tank_control_pkg/tank_control.py
@@ -1,7 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
-import RPi.GPIO as GPIO
+import lgpio as GPIO
 
 class TankControl(Node):
 
@@ -13,13 +13,18 @@ class TankControl(Node):
         self.right_motor_pins = [19, 26, 13]  # Forward, Reverse, PWM
 
         try:
-            GPIO.setmode(GPIO.BCM)
-            GPIO.setup(self.left_motor_pins, GPIO.OUT)
-            GPIO.setup(self.right_motor_pins, GPIO.OUT)
+            # Initialize the GPIO library
+            GPIO.set_internal_socket_port(8888)
+            GPIO.set_debug_level(0)
+            GPIO.initialize()
+
+            # Set up the left and right motor pins as outputs
+            for pin in self.left_motor_pins + self.right_motor_pins:
+                GPIO.set_mode(pin, GPIO.OUTPUT)
 
             # Set up the PWM for the left and right motors
-            self.left_pwm = GPIO.PWM(self.left_motor_pins[2], 2000)  # 2000Hz frequency
-            self.right_pwm = GPIO.PWM(self.right_motor_pins[2], 2000)  # 2000Hz frequency
+            self.left_pwm = GPIO.pwm(self.left_motor_pins[2], 2000)  # 2000Hz frequency
+            self.right_pwm = GPIO.pwm(self.right_motor_pins[2], 2000)  # 2000Hz frequency
 
             # Start the PWM with 0% duty cycle
             self.left_pwm.start(0)
@@ -33,89 +38,47 @@ class TankControl(Node):
         self.subscription = self.create_subscription(
             String,
             '/turtle1/cmd_vel',
-            self.listener_callback,
-            10
-        )
+            self.cmd_vel_callback,
+            10)
+        self.subscription  # prevent unused variable warning
 
-    def listener_callback(self, msg):
-        # Access the linear and angular velocity from the message data
+    def cmd_vel_callback(self, msg):
+        # Parse the linear and angular velocities from the message
         linear = msg.linear.x
         angular = msg.angular.z
 
-        # Control the motors based on the linear and angular velocity
-        if linear > 0:  # move forward
-            self.drive(self.left_motor_pins, True, False, 100)  # 100% duty cycle for full speed
-            self.drive(self.right_motor_pins, True, False, 100)
-        elif linear < 0:  # move backward
-            self.drive(self.left_motor_pins, False, True, 100)
-            self.drive(self.right_motor_pins, False, True, 100)
-        elif angular > 0:  # turn/spin left
-            self.drive(self.left_motor_pins, False, True, 100)
-            self.drive(self.right_motor_pins, True, False, 100)
-        elif angular < 0:  # turn/spin right
-            self.drive(self.left_motor_pins, True, False, 100)
-            self.drive(self.right_motor_pins, False, True, 100)
+        # Calculate the left and right motor speeds based on the velocities
+        left_speed = linear - angular
+        right_speed = linear + angular
+
+        # Set the left and right motor speeds using PWM
+        self.left_pwm.ChangeDutyCycle(abs(left_speed) * 100)
+        self.right_pwm.ChangeDutyCycle(abs(right_speed) * 100)
+
+        # Set the direction of the left and right motors based on the sign of the speeds
+        if left_speed >= 0:
+            GPIO.write(self.left_motor_pins[0], GPIO.LOW)
+            GPIO.write(self.left_motor_pins[1], GPIO.HIGH)
         else:
-            self.stop_motors()
+            GPIO.write(self.left_motor_pins[0], GPIO.HIGH)
+            GPIO.write(self.left_motor_pins[1], GPIO.LOW)
 
-    def drive(self, motor_pins, forward, reverse, speed):
-        # Set the motor pins and PWM duty cycle
-        try:
-            GPIO.output(motor_pins[0], forward)
-            GPIO.output(motor_pins[1], reverse)
-            pwm = motor_pins[2]
-            pwm.ChangeDutyCycle(speed)
-
-        except Exception as e:
-            self.get_logger().error('Error setting motor pins: %s' % str(e))
-            raise
-
-    def stop_motors(self):
-        # Stop the motors and clean up the GPIO pins
-        try:
-            GPIO.output(self.left_motor_pins[0], False)
-            GPIO.output(self.left_motor_pins[1], False)
-            GPIO.output(self.right_motor_pins[0], False)
-            GPIO.output(self.right_motor_pins[1], False)
-            self.left_pwm.ChangeDutyCycle(0)
-            self.right_pwm.ChangeDutyCycle(0)
-
-        except Exception as e:
-            self.get_logger().error('Error stopping motors: %s' % str(e))
-            raise
-
-    def on_shutdown(self):
-        # Stop the motors and clean up the GPIO pins when the node is shut down
-        try:
-            self.stop_motors()
-            GPIO.cleanup()
-
-        except Exception as e:
-            self.get_logger().error('Error cleaning up GPIO pins: %s' % str(e))
-            raise
+        if right_speed >= 0:
+            GPIO.write(self.right_motor_pins[0], GPIO.LOW)
+            GPIO.write(self.right_motor_pins[1], GPIO.HIGH)
+        else:
+            GPIO.write(self.right_motor_pins[0], GPIO.HIGH)
+            GPIO.write(self.right_motor_pins[1], GPIO.LOW)
 
 def main(args=None):
-    # Initialize the ROS2 node
-    try:
-        rclpy.init(args=args)
+    rclpy.init(args=args)
 
-        # Create an instance of the TankControl class
-        node = TankControl()
+    tank_control = TankControl()
 
-        # Spin the node until it is shut down
-        try:
-            rclpy.spin(node)
+    rclpy.spin(tank_control)
 
-        finally:
-            # Stop the motors and clean up the GPIO pins when the node is shut down
-            node.on_shutdown()
-
-            # Destroy the node and shut down ROS2
-            node.destroy_node()
-            rclpy.shutdown()
-
-    except Exception as e:
-        print('Error initializing ROS2 node: %s' % str(e))
+    tank_control.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
