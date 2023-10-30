b3946ff31acd9463443c568e1eebe298411a1647
Jeff Higgason <jhiggy@gmail.com>
2023-10-30 15:26:11 -0700

    deleted build folders

--- build/led_control_pkg/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/jeffh/ros2_ws/install/led_control_pkg'
--- build/tank_control_pkg/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/jeffh/ros2_ws/install/tank_control_pkg'
