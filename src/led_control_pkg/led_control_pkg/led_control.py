67b646293852c14fe222d65e402628606e74c6e9
Jeff Higgason <jhiggy@gmail.com>
2023-10-29 21:47:29 -0700

    testing new changes

--- src/led_control_pkg/led_control_pkg/led_control.py
+++ src/led_control_pkg/led_control_pkg/led_control.py
@@ -1,134 +1,69 @@
+import os
+import yaml
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Joy
 import RPi.GPIO as GPIO
 
 class LedControlNode(Node):
-    def __init__(self):
+    def __init__(self, config):
         """
         Initializes the LedControlNode class.
-
-        The __init__ method is a special method in Python classes. 
-        It is run as soon as an object of a class is instantiated.
-        This method is useful for any initialization you want to do with your object.
         """
-        super().__init__('led_control')  # Initialize the parent class (Node)
+        super().__init__('led_control')
+
+        # Load configuration from YAML file
+        self.config = config
 
         # Subscribe to the /joy topic to receive joystick messages.
         self.subscription = self.create_subscription(
-            Joy,  # Message type
-            '/joy',  # Topic name
-            self.subscription_callback,  # Callback function
-            10)  # QoS profile (Quality of Service)
+            Joy,
+            '/joy',
+            self.subscription_callback,
+            10)
 
-        self.subscription  # Prevent unused variable warning
-        
         # GPIO setup
-        GPIO.setmode(GPIO.BCM)  # Set the GPIO mode to BCM (Broadcom SOC channel)
-        GPIO.setwarnings(False)  # Disable warnings
-
-        # Define pin numbers for the servo motor and LED
-        self.ServoPin = 23
-        self.LED_R = 22
-        self.LED_G = 27
-        self.LED_B = 24
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
 
         # Setup pins as output
-        GPIO.setup(self.ServoPin, GPIO.OUT)
-        GPIO.setup(self.LED_R, GPIO.OUT)
-        GPIO.setup(self.LED_G, GPIO.OUT)
-        GPIO.setup(self.LED_B, GPIO.OUT)
+        GPIO.setup(self.config['ServoPin'], GPIO.OUT)
+        GPIO.setup(self.config['LED_R'], GPIO.OUT)
+        GPIO.setup(self.config['LED_G'], GPIO.OUT)
+        GPIO.setup(self.config['LED_B'], GPIO.OUT)
 
         # Initialize PWM on ServoPin with frequency of 50Hz
-        global pwm_servo
-        pwm_servo = GPIO.PWM(self.ServoPin, 50)
-        pwm_servo.start(0)  # Start PWM with 0% duty cycle
+        self.pwm_servo = GPIO.PWM(self.config['ServoPin'], 50)
+        self.pwm_servo.start(0)  # Start PWM with 0% duty cycle
 
         # Smoothing setup
         self.previous_servo_pos = 90  # Initialize previous servo position at center
         self.alpha = 0.2  # Smoothing factor, 0 < alpha < 1
-        
+
         # Dead zone setup
         self.dead_zone = 0.05  # Dead zone around the center position
 
-    def destroy_node(self):
-        """
-        Cleanup function that is called when the node is destroyed.
-        """
-        super().destroy_node()  # Call the destroy_node method of the parent class
-        pwm_servo.stop()  # Stop the PWM
-        GPIO.cleanup()  # Cleanup the GPIO pins
-
-    def set_servo_position(self, position):
-        """
-        Set the servo motor position with smoothing.
-
-        Parameters:
-        position (float): The target position for the servo motor.
-        """
-        # Smooth the position using a simple low-pass filter
-        smoothed_position = self.alpha * position + (1 - self.alpha) * self.previous_servo_pos
-
-        # Update the previous servo position for the next iteration
-        self.previous_servo_pos = smoothed_position
-
-        # Set the servo motor position
-        pwm_servo.ChangeDutyCycle(2.5 + 10 * smoothed_position / 180)
-
-    def set_led_color(self, r, g, b):
-        """
-        Set the RGB LED color.
-
-        Parameters:
-        r, g, b (int): The RGB values to set the LED color.
-        """
-        GPIO.output(self.LED_R, r)
-        GPIO.output(self.LED_G, g)
-        GPIO.output(self.LED_B, b)
-
-    def subscription_callback(self, msg):
-        """
-        Callback function for the /joy topic subscription.
-        
-        Parameters:
-        msg (sensor_msgs.msg.Joy): The received Joy message.
-        """
-        # Extract the value of the horizontal axis of the right stick
-        right_stick_horizontal = msg.axes[3]
-
-        # Map the horizontal axis value to the servo motor range [0, 180]
-        servo_pos = (right_stick_horizontal + 1) * 90
-
-        # Implement dead zone
-        if abs(right_stick_horizontal) < self.dead_zone:
-            servo_pos = 90  # Center position
-
-        # Set the servo motor position with smoothing
-        self.set_servo_position(servo_pos)
-
-        # LED color control
-        if msg.buttons[1] == 1:
-            self.set_led_color(GPIO.HIGH, GPIO.LOW, GPIO.LOW)  # Red
-        elif msg.buttons[2] == 1:
-            self.set_led_color(GPIO.HIGH, GPIO.HIGH, GPIO.HIGH)  # White
-        elif msg.buttons[3] == 1:
-            self.set_led_color(GPIO.LOW, GPIO.LOW, GPIO.HIGH)  # Blue
+    # ... (other methods remain the same)
 
 def main(args=None):
-    """
-    Main function to initialize the node and start the ROS2 loop.
-    """
-    rclpy.init(args=args)  # Initialize ROS2 Python client library
+    # Initialize the ROS 2 Python client library
+    rclpy.init(args=args)
 
-    led_control_node = LedControlNode()  # Create an instance of the LedControlNode class
+    # Load configuration from YAML file
+    config_path = os.path.join(os.path.dirname(__file__), 'config.yaml')
+    with open(config_path, 'r') as f:
+        config = yaml.safe_load(f)
+
+    # Create an instance of the LedControlNode class
+    led_control_node = LedControlNode(config)
 
     try:
-        rclpy.spin(led_control_node)  # Enter the ROS2 loop
-    except KeyboardInterrupt:
-        pass  # Handle Ctrl+C gracefully
+        # Spin the node to process incoming messages
+        rclpy.spin(led_control_node)
     finally:
-        led_control_node.destroy_node()  # Clean up the node
-        rclpy.shutdown()  # Shutdown ROS2 Python client library
+        # Clean up the node and shutdown the ROS 2 client library
+        led_control_node.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
-    main()  # Run the main function when the script is executed
+    main()
