ef24ba8d188f9ef4f1ea45b7de8e0464de548298
Jeff Higgason <jhiggy@gmail.com>
2023-10-29 18:22:34 -0700

    build

--- install/led_control_pkg/lib/python3.10/site-packages/led_control_pkg/led_control.py
+++ install/led_control_pkg/lib/python3.10/site-packages/led_control_pkg/led_control.py
@@ -1,103 +1,84 @@
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Joy
 import RPi.GPIO as GPIO
 
-class ServoControl(Node):
+class ServoControlNode(Node):
     """
-    This class defines the ServoControl node which listens to the /cmd_vel topic
-    and moves the servo motor based on the angular.z value of the Twist message.
+    A ROS2 node for controlling a servo motor using Joy messages from an Xbox controller.
     """
+    
     def __init__(self):
-        """Initialize the ServoControl node."""
-        super().__init__('servo_control')
+        """
+        Initialize the ServoControlNode.
+        """
+        super().__init__('servo_control_node')
 
-        # Define the GPIO pin for the servo motor.
+        # Definition of servo pin
         self.servo_pin = 23
         
         # Set the GPIO port to BCM encoding mode.
         GPIO.setmode(GPIO.BCM)
 
-        # Ignore warning information.
+        # Ignore warning information
         GPIO.setwarnings(False)
 
-        # Initialize the servo motor pin.
+        # Servo pin is initialized into output mode
         GPIO.setup(self.servo_pin, GPIO.OUT)
-        
-        # Initialize the PWM for the servo motor with 50Hz frequency.
         self.pwm_servo = GPIO.PWM(self.servo_pin, 50)
         self.pwm_servo.start(0)
 
-        # Subscribe to the /cmd_vel topic with Twist message type.
-        self.subscription = self.create_subscription(
-            Twist,
-            '/cmd_vel',
+        # Create a subscriber for /joy topic with callback function 'subscription_callback'.
+        self.subscriber = self.create_subscription(
+            Joy,
+            '/joy',
             self.subscription_callback,
             10,
         )
+        
+        # Log information to the console.
+        self.get_logger().info('Servo control node has been started.')
 
     def subscription_callback(self, msg):
         """
-        Callback function for the /cmd_vel topic subscription.
+        Callback function for the /joy topic subscription.
         
         Parameters:
-        msg (geometry_msgs.msg.Twist): The received Twist message.
+        msg (sensor_msgs.msg.Joy): The received Joy message.
         """
-        # Extract the angular.z value from the Twist message.
-        angular_z = msg.angular.z
+        # Extract the value of the first axis (left stick vertical) from the Joy message.
+        axis_value = msg.axes[1]
 
-        # Map the angular.z value to the servo motor range [0, 180].
-        servo_pos = (angular_z + 1) * 90
+        # Map the axis value to the servo motor range [0, 180].
+        servo_pos = (axis_value + 1) * 90
 
         # Set the servo motor position.
         self.set_servo_position(servo_pos)
 
-    def set_servo_position(self, position):
+    def set_servo_position(self, pos):
         """
         Set the position of the servo motor.
         
         Parameters:
-        position (float): The desired position of the servo motor in degrees.
+        pos (float): The desired position of the servo motor in degrees.
         """
-        # Ensure the position is within the valid range [0, 180].
-        position = max(0, min(position, 180))
-
-        # Calculate the duty cycle for the given position.
-        duty_cycle = 2.5 + 10 * position / 180
-
-        # Change the PWM duty cycle to set the servo position.
+        duty_cycle = 2.5 + 10 * pos / 180
         self.pwm_servo.ChangeDutyCycle(duty_cycle)
 
-    def on_shutdown(self):
-        """Cleanup the GPIO pins and stop the PWM on shutdown."""
-        try:
-            # Stop the PWM and clean up the GPIO pins.
-            self.pwm_servo.stop()
-            GPIO.cleanup()
-
-        except Exception as e:
-            self.get_logger().error(f'Error cleaning up GPIO pins: {e}')
-            raise
 
 def main(args=None):
-    """Main function to initialize and run the ServoControl node."""
-    try:
-        # Initialize the ROS2 node.
-        rclpy.init(args=args)
-        node = ServoControl()
+    rclpy.init(args=args)
 
-        try:
-            # Spin the node until it is shut down.
-            rclpy.spin(node)
+    servo_control_node = ServoControlNode()
 
-        finally:
-            # Stop the PWM and clean up the GPIO pins before shutting down.
-            node.on_shutdown()
-            node.destroy_node()
-            rclpy.shutdown()
+    rclpy.spin(servo_control_node)
 
-    except Exception as e:
-        print(f'Error initializing ROS2 node: {e}')
+    # Cleanup and shutdown.
+    servo_control_node.pwm_servo.stop()
+    GPIO.cleanup()
+    servo_control_node.destroy_node()
+    rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
