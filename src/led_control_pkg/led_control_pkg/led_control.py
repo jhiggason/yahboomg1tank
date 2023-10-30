b5c7fb739fd504cbb752ff9fb0a4296026a598c3
Jeff Higgason <jhiggy@gmail.com>
2023-10-29 20:45:34 -0700

    added in LED control with buttons on the cont

--- src/led_control_pkg/led_control_pkg/led_control.py
+++ src/led_control_pkg/led_control_pkg/led_control.py
@@ -17,7 +17,13 @@ class LedControlNode(Node):
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
         self.ServoPin = 23
+        self.LED_R = 22
+        self.LED_G = 27
+        self.LED_B = 24
         GPIO.setup(self.ServoPin, GPIO.OUT)
+        GPIO.setup(self.LED_R, GPIO.OUT)
+        GPIO.setup(self.LED_G, GPIO.OUT)
+        GPIO.setup(self.LED_B, GPIO.OUT)
         global pwm_servo
         pwm_servo = GPIO.PWM(self.ServoPin, 50)
         pwm_servo.start(0)
@@ -45,6 +51,17 @@ class LedControlNode(Node):
         # Set the servo motor position
         pwm_servo.ChangeDutyCycle(2.5 + 10 * smoothed_position / 180)
 
+    def set_led_color(self, r, g, b):
+        """
+        Set the RGB LED color.
+
+        Parameters:
+        r, g, b (int): The RGB values to set the LED color.
+        """
+        GPIO.output(self.LED_R, r)
+        GPIO.output(self.LED_G, g)
+        GPIO.output(self.LED_B, b)
+
     def subscription_callback(self, msg):
         """
         Callback function for the /joy topic subscription.
@@ -52,18 +69,26 @@ class LedControlNode(Node):
         Parameters:
         msg (sensor_msgs.msg.Joy): The received Joy message.
         """
-        # Extract the value of the horizontal axis of the right stick
-        right_stick_horizontal = msg.axes[3]
+        # Extract the value of the horizontal axis of the left stick
+        left_stick_horizontal = msg.axes[0]
 
         # Map the horizontal axis value to the servo motor range [0, 180]
-        servo_pos = (right_stick_horizontal + 1) * 90
+        servo_pos = (left_stick_horizontal + 1) * 90
 
         # Implement dead zone
-        if abs(right_stick_horizontal) < self.dead_zone:
+        if abs(left_stick_horizontal) < self.dead_zone:
             servo_pos = 90  # Center position
 
         # Set the servo motor position with smoothing
         self.set_servo_position(servo_pos)
+
+        # LED color control
+        if msg.buttons[0] == 1:
+            self.set_led_color(GPIO.HIGH, GPIO.LOW, GPIO.LOW)  # Red
+        elif msg.buttons[1] == 1:
+            self.set_led_color(GPIO.HIGH, GPIO.HIGH, GPIO.HIGH)  # White
+        elif msg.buttons[2] == 1:
+            self.set_led_color(GPIO.LOW, GPIO.LOW, GPIO.HIGH)  # Blue
 
 def main(args=None):
     rclpy.init(args=args)
